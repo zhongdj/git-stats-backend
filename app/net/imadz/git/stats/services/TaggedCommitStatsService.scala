66867cb0d1236267b726aead63afdbaeb92667ef
zhongdejian <zhongdejian@didichuxing.com>
2019-06-12 22:25:05 +0800

    add golang project tagging method

--- app/net/imadz/git/stats/services/TaggedCommitStatsService.scala
+++ app/net/imadz/git/stats/services/TaggedCommitStatsService.scala
@@ -1,11 +1,11 @@
 package net.imadz.git.stats.services
 
 import com.google.inject.Inject
-import net.imadz.git.stats.{ AppError, ShellCommandExecError }
-import play.api.db.slick.{ DatabaseConfigProvider, HasDatabaseConfigProvider }
+import net.imadz.git.stats.{AppError, ShellCommandExecError}
+import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
 import slick.jdbc.JdbcProfile
 
-import scala.concurrent.{ ExecutionContext, Future }
+import scala.concurrent.{ExecutionContext, Future}
 import scala.language.postfixOps
 import scala.sys.process._
 
@@ -13,7 +13,7 @@ case class CommitOnFile(id: String, file: String, developer: String, date: Strin
 
 case class TaggedCommit(tag: String, commit: CommitOnFile)
 
-class TaggedCommitStatsService @Inject() (protected val dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext)
+class TaggedCommitStatsService @Inject()(protected val dbConfigProvider: DatabaseConfigProvider)(implicit ec: ExecutionContext)
   extends Traversal with Constants with HasDatabaseConfigProvider[JdbcProfile] {
 
   def exec(dir: String): Either[AppError, Future[String]] = {
@@ -67,19 +67,25 @@ class TaggedCommitStatsService @Inject() (protected val dbConfigProvider: Databa
     ).map(_ => xs)
   }
 
-  private def srcMain: CommitOnFile => Boolean = commit => commit.file.contains("src/main")
+  private def srcMain: CommitOnFile => Boolean = commit =>
+    commit.file.contains("src/main") || commit.file.contains("app/cmd")
 
-  private def infrastructure: CommitOnFile => Option[String] = commit => if (commit.file.contains("infrastructure/")) Some("infrastructure") else None
+  private def infrastructure: CommitOnFile => Option[String] = commit =>
+    if (commit.file.contains("infrastructure/")) Some("infrastructure") else None
 
-  private def application: CommitOnFile => Option[String] = commit => if (commit.file.contains("application/")) Some("application") else None
+  private def application: CommitOnFile => Option[String] = commit =>
+    if (commit.file.contains("application/")) Some("application") else None
 
-  private def domain: CommitOnFile => Option[String] = commit => if (commit.file.contains("domain/")) Some("domain") else None
+  private def domain: CommitOnFile => Option[String] = commit =>
+    if (commit.file.contains("domain/") || commit.file.contains("app/db") || commit.file.contains("app/service/async/jobs")) Some("domain") else None
 
   private def controller: CommitOnFile => Option[String] = commit => Some("controller").filter(commit.file.contains)
 
-  private def gateway: CommitOnFile => Option[String] = commit => Some("gateway").filter(commit.file.contains)
+  private def gateway: CommitOnFile => Option[String] = commit =>
+    if (commit.file.contains("gateway") || commit.file.contains("app/service/remote")) Some("gateway") else None
 
-  private def configuration: CommitOnFile => Option[String] = commit => Some("configuration").filter(commit.file.contains)
+  private def configuration: CommitOnFile => Option[String] = commit =>
+    if (commit.file.contains("configuration") || commit.file.contains("app/configs")) Some("configuration") else None
 
   private def tags: CommitOnFile => List[String] = commit =>
     List(infrastructure, application, domain, controller, gateway, configuration)
