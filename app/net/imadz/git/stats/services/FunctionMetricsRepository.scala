76fadd1f523716b38fbadba241214914fd0d4bad
Barry Zhong <zhongdj@gmail.com>
2020-09-06 01:58:05 +0800

    Fix: Using jdbc batch and add item id into unique contraints

--- app/net/imadz/git/stats/services/FunctionMetricsRepository.scala
+++ app/net/imadz/git/stats/services/FunctionMetricsRepository.scala
@@ -3,7 +3,7 @@ package net.imadz.git.stats.services
 import java.sql.Date
 
 import com.google.inject.Inject
-import net.imadz.git.stats.models
+import net.imadz.git.stats.models.Tables
 import net.imadz.git.stats.models.Tables.FuncMetricRow
 import net.imadz.git.stats.services.GolangFuncsParser.FuncMetric
 import play.api.db.slick.{ DatabaseConfigProvider, HasDatabaseConfigProvider }
@@ -17,14 +17,15 @@ class FunctionMetricsRepository @Inject() (protected val dbConfigProvider: Datab
 
   import dbConfig.profile.api._
 
-  val functionMetrics = TableQuery[models.Tables.FuncMetric]
-
   def toRow(taskId: Int, taskItemId: Int, projectRoot: String, day: Date): FuncMetric => FuncMetricRow = fm => FuncMetricRow(0, taskId, taskItemId, day, fm.name, fm.abbrPath, projectRoot, fm.abbrPath, fm.lines, fm.params, fm.complexity, fm.complexityRate.toFloat)
 
   def save(taskId: Int, taskItemId: Int, projectRoot: String, now: Date, metrics: List[FuncMetric]): Future[List[Int]] = {
+    val q = Tables.FuncMetric.returning(Tables.FuncMetric.map(_.id))
     val rows: List[FuncMetricRow] = metrics.map(toRow(taskId, taskItemId, projectRoot, now))
-    rows.foreach(println)
-    dbConfig.db.run(functionMetrics.returning(functionMetrics.map(_.id)) ++= rows)
+    dbConfig.db.run(DBIO.sequence(
+      rows.map(row => {
+        q.insertOrUpdate(row).map(_.getOrElse(0))
+      })))
       .map(_.toList)
   }
 
--- conf/evolutions/default/6.sql
+++ conf/evolutions/default/6.sql
@@ -1,7 +1,7 @@
 -- function metrics
 
 -- !Ups
-ALTER TABLE func_metric ADD CONSTRAINT t_d_n_p UNIQUE (`task_id`,`day`,`name`,`path`);
+ALTER TABLE func_metric ADD CONSTRAINT t_d_n_p UNIQUE (`task_id`,`task_item_id`, `day`,`name`,`path`);
 
 -- !Downs
 ALTER TABLE func_metric DROP CONSTRAINT t_d_n_p;
